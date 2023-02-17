b66e3556d60657449cc6e301948ba56a2219364d
William Thistlethwaite <williamthistle@gmail.com>
2023-02-17 09:56:59 -0500

    Update wrapper.R

--- R/wrapper.R
+++ R/wrapper.R
@@ -1,13 +1,13 @@
 #' Wrapper function for running the SPEEDI pipeline
 #'
+#' @param tissue Tissue of data (used to map cell types via reference)
 #' @param data_path path to where data is located
 #' @param sample_id_list list of sample names (optional - if not provided, will select all samples found recursively in data_path)
-#' @param tissue Tissue of data (used to map cell types via reference)
 #' @param human flag to indicate whether we're processing human or mouse data
 #' @param remove_doublets flag to indicate whether we're removing doublets from the data (using scDblFinder)
 #' @return A Seurat object that has been processed through the SPEEDI pipeline
 #' @export
-run_SPEEDI <- function(data_path, sample_id_list, tissue, human = TRUE, remove_doublets = FALSE) {
+run_SPEEDI <- function(tissue, data_path = getwd(), sample_id_list = NULL, human = TRUE, remove_doublets = FALSE) {
   all_sc_exp_matrices <- Read_h5(data_path, sample_id_list)
   sc_obj <- FilterRawData(all_sc_exp_matrices, human, remove_doublets)
   rm(all_sc_exp_matrices)
