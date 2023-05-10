2cb346d483d61b459aa54cee30ccb1595fcbff5f
William Thistlethwaite <williamthistle@gmail.com>
2023-05-10 15:54:33 -0400

    Update aaa.R

--- R/aaa.R
+++ R/aaa.R
@@ -1,5 +1,48 @@
+# Environment to store global package variables
+the <- new.env(parent = emptyenv())
+
+# URL for PBMC reference - if the link becomes defunct and we haven't updated it yet,
+# update the variable below (either by the setter below or by directly editing this file)
+the$pbmc_reference_url <- "https://atlas.fredhutch.org/data/nygc/multimodal/pbmc_multimodal.h5seurat"
+
+# List of possible SeuratData references
+the$seuratdata_references <- c("adiposeref", "bonemarrowref", "fetusref",
+                               "heartref", "humancortexref", "kidneyref",
+                               "lungref", "pancreasref", "pbmcref", "tonsilref", "mousecortexref")
+
+#' Get PBMC reference URL
+#' @export
+get_pbmc_reference_url <- function() {
+  the$pbmc_reference_url
+}
+
+#' Set PBMC reference URL
+#' @param new_url the new PBMC reference url
+#' @export
+set_pbmc_reference_url <- function(new_url = "https://atlas.fredhutch.org/data/nygc/multimodal/pbmc_multimodal.h5seurat") {
+  old <- the$pbmc_reference_url
+  the$pbmc_reference_url <- new_url
+  invisible(old)
+}
+
+#' Get possible SeuratData references
+#' @export
+get_seuratdata_references <- function() {
+  the$seuratdata_references
+}
+
+# We use the packages below, but not with direct calls in our functions.
+# Thus, to avoid check() complaining about us not using these packages in
+# its import check, we add some direct calls in the (never used) function below
+ignore_unused_imports <- function() {
+  hdf5r::as_hex
+  Matrix::Arith
+  glmGamPoi::glm_gp
+  RCurl::AUTH_ANY
+}
+
 ####################################################################
-# Gene Integration Matrix Methods
+# There is a bug in the original ArchR addGeneIntegrationMatrix function, so we create our own version here.
 ####################################################################
 
 #' Add a GeneIntegrationMatrix to ArrowFiles or an ArchRProject
@@ -831,47 +874,7 @@ addGeneIntegrationMatrix_SPEEDI <- function(
 
 }
 
-# Environment to store global package variables
-the <- new.env(parent = emptyenv())
-
+# Add our new version of the addGeneIntegrationMatrix function to the ArchR namespace
+# This makes it so we don't have to define the namespace of all of the ArchR functions
+# inside the function
 environment(addGeneIntegrationMatrix_SPEEDI) <- asNamespace('ArchR')
-
-# URL for PBMC reference - if the link becomes defunct and we haven't updated it yet,
-# update the variable below (either by the setter below or by directly editing this file)
-the$pbmc_reference_url <- "https://atlas.fredhutch.org/data/nygc/multimodal/pbmc_multimodal.h5seurat"
-
-# List of possible SeuratData references
-the$seuratdata_references <- c("adiposeref", "bonemarrowref", "fetusref",
-                               "heartref", "humancortexref", "kidneyref",
-                               "lungref", "pancreasref", "pbmcref", "tonsilref", "mousecortexref")
-
-#' Get PBMC reference URL
-#' @export
-get_pbmc_reference_url <- function() {
-  the$pbmc_reference_url
-}
-
-#' Set PBMC reference URL
-#' @param new_url the new PBMC reference url
-#' @export
-set_pbmc_reference_url <- function(new_url = "https://atlas.fredhutch.org/data/nygc/multimodal/pbmc_multimodal.h5seurat") {
-  old <- the$pbmc_reference_url
-  the$pbmc_reference_url <- new_url
-  invisible(old)
-}
-
-#' Get possible SeuratData references
-#' @export
-get_seuratdata_references <- function() {
-  the$seuratdata_references
-}
-
-# We use the packages below, but not with direct calls in our functions.
-# Thus, to avoid check() complaining about us not using these packages in
-# its import check, we add some direct calls in the (never used) function below
-ignore_unused_imports <- function() {
-  hdf5r::as_hex
-  Matrix::Arith
-  glmGamPoi::glm_gp
-  RCurl::AUTH_ANY
-}
