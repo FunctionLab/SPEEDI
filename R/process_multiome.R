b65777817e1cd1e9eb99fb9897f52b607781206c
William Thistlethwaite <williamthistle@gmail.com>
2023-05-23 15:32:09 -0400

    Update process_multiome.R

--- R/process_multiome.R
+++ R/process_multiome.R
@@ -44,13 +44,13 @@ FindMultiomeOverlap <- function(sc_obj, proj, data_modality = "RNA", log_flag = 
 #' \dontrun{proj <- FindMultiomeOverlap(sc_obj, proj, data_modality = "ATAC")}
 #' @export
 TransferRNALabels <- function(sc_obj, proj, log_flag = FALSE) {
-  #print_SPEEDI("\n", log_flag, silence_time = TRUE)
-  #print_SPEEDI("Step 10: Transferring cell type labels from RNA data to ATAC data (true multiome)", log_flag)
+  print_SPEEDI("\n", log_flag, silence_time = TRUE)
+  print_SPEEDI("Step 10: Transferring cell type labels from RNA data to ATAC data (true multiome)", log_flag)
   proj <- ArchR::addCellColData(ArchRProj = proj, data = as.character(sc_obj$predicted_celltype_majority_vote), cells = proj$cellNames, name = "Cell_type_voting_RNA", force = TRUE)
   p1 <- ArchR::plotEmbedding(ArchRProj = proj, colorBy = "cellColData", name = "Cell_type_voting_RNA", embedding = "UMAP", force = TRUE, keepAxis = TRUE)
   p2 <- ArchR::plotEmbedding(ArchRProj = proj, colorBy = "cellColData", name = "Cell_type_voting", embedding = "UMAP", force = TRUE, keepAxis = TRUE)
   ArchR::plotPDF(p1,p2, name = "UMAP_multiome_RNA_labels_and_original_labels_plots", ArchRProj = proj, addDOC = FALSE, width = 5, height = 5)
-  #print_SPEEDI("Step 10: Complete", log_flag)
+  print_SPEEDI("Step 10: Complete", log_flag)
   gc()
   return(proj)
 }
